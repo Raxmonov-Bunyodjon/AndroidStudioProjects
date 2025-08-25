494f609dd5c05c9a6e0b1fb88e64a63dc39f8d55
Bunyodjon Raxmonov <raxmonovbunyodjon5081999@gmail.com>
2025-08-25 10:08:02 +0500

    Initial commit: Registration form UI

---
+++ AndroidBesicLesson5/app/src/main/java/uz/bbb/androidbesiclesson5/MainActivity.kt
@@ -0,0 +1,14 @@
+package uz.bbb.androidbesiclesson5
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tasks_1)
+    }
+}
